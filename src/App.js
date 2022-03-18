b04ac350b58258c2be7d2cfe6cf88942a7d3b139
Patricia <patricia.benites.cava@gmail.com>
2022-03-18 14:02:41 +0100

    add iteration 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+const contacts = require('./contacts.json').slice(0,5)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>IronContacts</h1>
+    <table className='table'>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+      {contacts.map((contact) => (
+        <tr>
+        <td><img width="100px" src={contact.pictureUrl}/></td>
+        <td>{contact.name}</td>
+        <td>{contact.popularity}</td>
+      </tr>
+      ))}
+      
+    </table>
+
     </div>
   );
 }
