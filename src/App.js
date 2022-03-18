94e1fea6668e4cf27b9805dd7751dc57145560ab
Patricia <patricia.benites.cava@gmail.com>
2022-03-18 15:07:10 +0100

    add iteration 4

--- src/App.js
+++ src/App.js
@@ -4,23 +4,41 @@ import { useState } from "react";
 const initialContacts = require("./contacts.json").slice(0, 5);
 const remainingContacts = require("./contacts.json").slice(5);
 
-
-
 function App() {
-  const [contacts, setContacts] = useState(initialContacts)
+  const [contacts, setContacts] = useState(initialContacts);
 
   const addContact = () => {
-      setContacts((contacts) => {
+    setContacts((contacts) => {
       return [
         ...contacts,
-        remainingContacts[Math.floor(Math.random()*remainingContacts.length)]
-      ]
-    })
-  }
+        remainingContacts[Math.floor(Math.random() * remainingContacts.length)],
+      ];
+    });
+  };
+
+  const sortPopularity = () => {
+    setContacts((contacts) => {
+      return [...contacts].sort((a, b) => {
+        return b.popularity - a.popularity;
+      });
+    });
+  };
+
+  const sortName = () => {
+    setContacts((contacts) => {
+      return [...contacts].sort((a, b) => {
+        if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+        if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+        return 0;
+      });
+    });
+  };
   return (
     <div className="App">
       <h1>IronContacts</h1>
-      <button onClick ={addContact} >Add Random Contact</button>
+      <button onClick={addContact}>Add Random Contact</button>
+      <button onClick={sortPopularity}>Sort by popularity</button>
+      <button onClick={sortName}>Sort by name</button>
       <table className="table">
         <tr>
           <th>Picture</th>
