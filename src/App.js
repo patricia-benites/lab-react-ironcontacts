0219b3dd3c77534b7f8b9ab95af7f124b5668ceb
Patricia <patricia.benites.cava@gmail.com>
2022-03-18 14:21:17 +0100

    add iteration 2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import './App.css';
+import "./App.css";
 
-
-const contacts = require('./contacts.json').slice(0,5)
+const contacts = require("./contacts.json").slice(0, 5);
 
 function App() {
   return (
     <div className="App">
-    <h1>IronContacts</h1>
-    <table className='table'>
-      <tr>
-        <th>Picture</th>
-        <th>Name</th>
-        <th>Popularity</th>
-      </tr>
-      {contacts.map((contact) => (
+      <h1>IronContacts</h1>
+      <table className="table">
         <tr>
-        <td><img width="100px" src={contact.pictureUrl}/></td>
-        <td>{contact.name}</td>
-        <td>{contact.popularity}</td>
-      </tr>
-      ))}
-      
-    </table>
-
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+        </tr>
+        {contacts.map((contact) => (
+          <tr>
+            <td>
+              <img width="100px" src={contact.pictureUrl} />
+            </td>
+            <td>{contact.name}</td>
+            <td>{contact.popularity}</td>
+            <td>{contact.wonOscar ? "trophy" : " "}</td>
+            <td>{contact.wonEmmy ? "trophy" : " "}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
